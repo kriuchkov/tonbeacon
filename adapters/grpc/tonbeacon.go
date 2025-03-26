848f4dcc7ce6d0edf342c29fded2d2b26469e9af
Nikita Kriuchkov <nkriuchkov@outlook.com>
2025-03-26 10:52:51 +0300

    fixup! feat(TB-4): grpc server

--- adapters/grpc/tonbeacon.go
+++ adapters/grpc/tonbeacon.go
@@ -1,17 +1,12 @@
 package grpc
 
 import (
-	"context"
 	"net"
 
-	"github.com/go-faster/errors"
-	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
 
 	pb "github.com/kriuchkov/tonbeacon/api/grpc/v1"
-	"github.com/kriuchkov/tonbeacon/core/model"
 	"github.com/kriuchkov/tonbeacon/core/ports"
 )
 
@@ -33,44 +28,4 @@ func (s *TonBeacon) Run(lis net.Listener) error {
 
 func (s *TonBeacon) Stop() {
 	s.server.Stop()
-}
-
-func (s *TonBeacon) CreateAccount(ctx context.Context, req *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
-	log.Debug().Str("account_id", req.GetAccountId()).Msg("create account")
-
-	account, err := s.accountSvc.CreateAccount(ctx, req.GetAccountId())
-	if err != nil {
-		if errors.Is(err, model.ErrAccountExists) {
-			return createAccountPbError(codes.AlreadyExists, err), nil
-		}
-
-		log.Err(err).Msg("create account")
-		return createAccountPbError(codes.Internal, errors.Wrap(err, "create account")), nil
-	}
-
-	pbAccount := pb.Account{
-		AccountId: account.ID,
-		WalletId:  account.WalletID,
-		Address:   account.Address.String(),
-	}
-	return &pb.CreateAccountResponse{Account: &pbAccount}, nil
-}
-
-func createAccountPbError(code codes.Code, err error) *pb.CreateAccountResponse {
-	return &pb.CreateAccountResponse{Error: &pb.Error{Code: uint32(code), Message: err.Error()}}
-}
-
-func (s *TonBeacon) CloseAccount(ctx context.Context, req *pb.CloseAccountRequest) (*pb.CloseAccountResponse, error) {
-	err := s.accountSvc.CloseAccount(ctx, req.GetAccountId())
-	if err != nil {
-		if errors.Is(err, model.ErrAccountNotFound) {
-			return closeAccountPbError(codes.NotFound, err), nil
-		}
-		return closeAccountPbError(codes.Internal, errors.Wrap(err, "close account")), nil
-	}
-	return &pb.CloseAccountResponse{}, nil
-}
-
-func closeAccountPbError(code codes.Code, err error) *pb.CloseAccountResponse {
-	return &pb.CloseAccountResponse{Error: &pb.Error{Code: uint32(code), Message: err.Error()}}
 }
